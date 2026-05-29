a51e48ee4e723aea821b267cf1812e0c9aed5fa6
GomesJ7 <98333692+GomesJ7@users.noreply.github.com>
2026-05-29 09:06:08 +0200

    feat new logo

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import logoESEO from "../../images/ESEO.jpeg";
+import logoESEO from "../../images/ESEO.png";
 import "../../css/Login.css";
 
 const Login = () => {
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import logoESEO from "../../images/ESEO.jpeg";
+import logoESEO from "../../images/ESEO.png";
 import "../../css/Home.css";
 
 const Home = () => {
